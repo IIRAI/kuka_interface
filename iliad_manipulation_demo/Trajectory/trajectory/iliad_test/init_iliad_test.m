e76671ac1738f5e12a16a827625a7ab9d97cf35c
Edoardo <edoardo.sorelli@gmail.com>
2019-05-29 12:52:04 +0200

    defined initial configuration robot

--- iliad_manipulation_demo/Trajectory/trajectory/iliad_test/init_iliad_test.m
+++ iliad_manipulation_demo/Trajectory/trajectory/iliad_test/init_iliad_test.m
@@ -2,22 +2,7 @@
 rosshutdown
 rosinit%('http://192.168.0.150:11311')
 
-%% HOMING: define home joints positions
-% home position
-load('q_0_left')
-load('q_0_right')
-
-% homing: from default to home poition
-load('q_out_left_home')
-load('q_out_right_home')
-
-q_left_home  = zeros(128, 2000);
-q_right_home = zeros(128, 2000);
-
-for i = 1:2000
-    q_left_home(1:7, i)  = q_out_left_home(:, i);
-    q_right_home(1:7, i) = q_out_right_home(:, i);
-end
+%% Simulink parameters
 
 % table of number of samples for the desired trajectory
 t_prova = [300,300;...
@@ -28,3 +13,14 @@ t_prova = [300,300;...
 
 % standard length (in samples) of the trajectory evaluated
 max_output_len = 4000;
+
+%% Initial configuration
+% avoid starting from singularity when usind rviz
+q_left_init  = [0.1, 0.1, 0.1, 0, 0,  1, -1.5]';
+q_right_init = [0.1, 0.1, 0.1, 0, 0, -1,    0]';
+
+%% HOMING: define home joints positions
+% home position
+load('q_0_left')
+load('q_0_right')
+
