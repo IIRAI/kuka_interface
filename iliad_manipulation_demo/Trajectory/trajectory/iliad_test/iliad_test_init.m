ba14e7a77cdc90b34045cb4a2ebba6b9d39faaf6
Edoardo <edoardo.sorelli@gmail.com>
2019-06-03 16:25:29 +0200

    enhanced cmd input management and defined new default pose

--- iliad_manipulation_demo/Trajectory/trajectory/iliad_test/iliad_test_init.m
+++ iliad_manipulation_demo/Trajectory/trajectory/iliad_test/iliad_test_init.m
@@ -31,15 +31,18 @@ pos_threshold = 0.1;
 q_left_init  = [0.1, 0.1, 0.1, 0, 0,  1, -1.5]';
 q_right_init = [0.1, 0.1, 0.1, 0, 0, -1,    0]';
 
-%% Define default position of the arms
-% table dimension from rviz model
-% length = 0.6 [m]
-% width  = 0.8 [m]
-% height = 0.7 [m]
+%% Define default position of the arms   ___________________
+% table dimension from rviz model        |    Table        |
+% length = 0.6 [m]                       ^    Plane        |
+% width  = 0.8 [m]                       |Y                |
+% height = 0.7 [m]                       |    X            |
+%                                        +----->___________|
 
 % deafault end_effectors position in table reference system
-ee_left_t_def  = [0.8, 0.1, 0.1, -pi/2, 0, 0]';
-ee_right_t_def = [0.0, 0.1, 0.1, -pi/2, 0, 0]';
+% ee_left_t_def  = [0.8, 0.1, 0.1, -pi/2, 0, 0]';
+% ee_right_t_def = [0.0, 0.1, 0.1, -pi/2, 0, 0]';
+ee_left_t_def  = [0.0, 0.1, 0.1, pi/2, 0, 0]';
+ee_right_t_def = [0.8, 0.1, 0.1, pi/2, 0, 0]';
 
 % default position and orientation of the `left_arm_7_link` and `right_arm_7_link`
 pose_left_def  = ee_2_left7link(ee_left_t_def);
--- iliad_manipulation_demo/Trajectory/trajectory/iliad_test/manage_cmd_input/activate_command.m
+++ iliad_manipulation_demo/Trajectory/trajectory/iliad_test/manage_cmd_input/activate_command.m
@@ -12,30 +12,16 @@ while ask_command
     
     cmd = input('iliad_test command: $ ', 's'); % wait for command
     
-    % interpret Icode
-    if strcmp(cmd(1), 'I')
-        switch cmd(2)
-            case '0' % homing
-                set_param('iliad_test/Icode','Value', '0');
-            case '1' 
-                I1; % go to position
-            otherwise
-                warning('Unexpected Icode, type h for help ¯\_(⊙︿⊙)_/¯ ')
-        end
+    if isempty(cmd)
+        set_param('iliad_test/enable','Value', '0');
+        warning('An empty input is interpreted as a `disable` command')
+        disp('type h for help (◠﹏◠)')
     else
-        
-    % interpret command
-        switch cmd
-            case 'h'         % print help
-                display_help;
-            case 'enable'    % turn on the arms
-                set_param('iliad_test/enable','Value', '1');
-            case 'disable'   % turn off the arms
-                set_param('iliad_test/enable','Value', '0');
-            case 'end'          
-                end_command; % exit the loop and set the default value for iliad_test
-            otherwise         
-                warning('Unexpected command, type h for help ¯\_(⊙︿⊙)_/¯ ')
+        % interpret Icode
+        if strcmp(cmd(1), 'I')
+            Icode_interpreter;
+        else % interpret command
+            cmd_interpreter;
         end
     end
    
