226f3d7c087f9db0277f462e3588dcf3903a82f1
Edoardo <edoardo.sorelli@gmail.com>
2019-12-13 15:02:09 +0100

    in iliad_ctrl set up the end-effectors when enabling or homing the robot

--- iliad_manipulation_demo/Trajectory/trajectory/iliad_test/manage_cmd_input/Icode_interpreter/Icode_interpreter.m
+++ iliad_manipulation_demo/Trajectory/trajectory/iliad_test/manage_cmd_input/Icode_interpreter/Icode_interpreter.m
@@ -10,7 +10,15 @@ else
     switch cmd(2)
         case '0' % homing
             set_param(sim_name + '/Icode','Value', '0');
-            open_hand;
+            if strcmp(sim_name, "iliad_test")
+                open_hand;
+            elseif strcmp(sim_name, "iliad_ctrl")
+                disp('setting up the end-effextors...')
+                % open hand command I6
+                set_param(sim_name + '/hand_synergy', 'Value', sprintf('%f', 0));
+                % unroll elvet tray
+                set_param(sim_name + '/velvet_ctrl', 'Value', sprintf('%f', 1));
+            end
         case '1' % go to position
             I1;
             set_param(sim_name + '/Icode','Value', '1');
@@ -22,10 +30,10 @@ else
         case '5'
             open_hand;
         case '6' %% TODO(ed): delete I4/I5 in favor to I6/I7
-            hand_syn = input(sim_name + ' command: $ hand synergy (0=open / 1=losed): ');
+            hand_syn = input(sim_name + ' command: $ hand synergy (0=open / 1=closed): ');
             set_param(sim_name + '/hand_synergy', 'Value', sprintf('%f', hand_syn));
         case '7'
-            velvet_ctrl = input(sim_name + ' command: $ velvet control (0=open / 1=losed): ');
+            velvet_ctrl = input(sim_name + ' command: $ velvet control (0=roll up / 1=unroll): ');
             set_param(sim_name + '/velvet_ctrl', 'Value', sprintf('%f', velvet_ctrl));
         otherwise
             warning('Unexpected Icode, type h for help ¯\_(⊙︿⊙)_/¯ ')
--- iliad_manipulation_demo/Trajectory/trajectory/iliad_test/manage_cmd_input/cmd_interpreter/cmd_interpreter.m
+++ iliad_manipulation_demo/Trajectory/trajectory/iliad_test/manage_cmd_input/cmd_interpreter/cmd_interpreter.m
@@ -9,6 +9,13 @@ switch cmd
         display_help;
     case 'enable'    % turn on the arms
         set_param(sim_name + '/enable','Value', '1');
+        if strcmp(sim_name, "iliad_ctrl")  % if real robot set up the ee too
+            disp('setting up the end-effextors...')
+            % open hand command I6
+            set_param(sim_name + '/hand_synergy', 'Value', sprintf('%f', 0));
+            % unroll elvet tray
+            set_param(sim_name + '/velvet_ctrl', 'Value', sprintf('%f', 1));
+        end
     case 'disable'   % turn off the arms
         set_param(sim_name + '/enable','Value', '0');
     case 'end'          
